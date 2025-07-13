fd4d51ab51b2f53436d46babc903501675436369
joaohgp-dev <joaohgpereira@gmail.com>
2025-07-13 00:57:54 -0300

    first commit

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import importlib
+import pandas as pd
 
 PAGES = {
     "Home": "streamlit_app.home",
     "Calculus": "streamlit_app.calculus.index",
     "Electronics": "streamlit_app.microelectronic.index"
 }
+
+chart_data = pd.DataFrame(
+    data=[
+        [1, 7, 4],
+        [2, 8, 5],
+        [3, 9, 6]
+    ],
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
 
 st.set_page_config(page_title="Embedded Journey", layout="wide")
 
---
+++ streamlit_app/home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def app():
+    st.title("Embedded Journey")
+    st.markdown("""
+    Bem-vindo ao repositório de estudos em Sistemas Embarcados!
+    Use o menu lateral para navegar entre os tópicos.
+
+    ### Áreas de estudo:
+    - 🧮 Cálculo
+    - 🔌 Microeletrônica
+
+    """)
